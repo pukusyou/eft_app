5e3ce1222d0714621e662285e63b7ab441eb7651
pukusyou <pukusho@gmail.com>
2023-01-29 22:59:12 +0900

    最新サーバー状態

--- static/js/task_item.js
+++ static/js/task_item.js
@@ -10,7 +10,7 @@ var driver = new Driver({
 
 driver.defineSteps([
   {
-    element: '.modal-open', // ハイライトさせる要素
+    element: '#setting', // ハイライトさせる要素
     popover: {
       title: '1.表示設定',
       description: 'アイテムの表示・非表示のオプションを選択できます',
@@ -19,62 +19,19 @@ driver.defineSteps([
    }
    ]);
 
-function toggle_element(classname) {
-        $(classname).toggle();
-}
 
 $(function(){
-	// 変数に要素を入れる
-	var open = $('.modal-open'),
-		close = $('.modal-close'),
-		container = $('.modal-container');
+  if ($("#fullname").text()=='MP-133') {
+    driver.start();
+  }
+});
 
-    if ($("#fullname").text()=='MP-133') {
-      driver.start();
-    }
-	//開くボタンをクリックしたらモーダルを表示する
-	open.on('click',function(){	
-		container.addClass('active');
-		return false;
-	});
-
-    $("#loot").on("click", function() {
-        $("#loot").toggleClass("checked");
-        toggle_element('.loot');
-        if(!$('#loot_in').prop("checked")) {
-          $("#loot input").prop("checked", true);
-        } else {
-          $("#loot input").prop("checked", false);
-        }
-      });
-      $("#key").on("click", function() {
-        $("#key").toggleClass("checked");
-        toggle_element('.key');
-        if(!$('#key_in').prop("checked")) {
-          $("#key input").prop("checked", true);
-        } else {
-          $("#key input").prop("checked", false);
-        }
-      });
-      $("#wepon").on("click", function() {
-        $("#wepon").toggleClass("checked");
-        toggle_element('.wepon');
-        if(!$('#wepon_in').prop("checked")) {
-          $("#wepon input").prop("checked", true);
-        } else {
-          $("#wepon input").prop("checked", false);
-        }
-      });
-
-	//閉じるボタンをクリックしたらモーダルを閉じる
-	close.on('click',function(){	
-		container.removeClass('active');
-	});
-
-	//モーダルの外側をクリックしたらモーダルを閉じる
-	$(document).on('click',function(e) {
-		if(!$(e.target).closest('.modal-body').length) {
-			container.removeClass('active');
-		}
-	});
+$(".toggle").on("click", function() {
+  $(this).toggleClass("checked");
+  $('.' + this.id).toggle();
+  if(!$(this).find("input").prop("checked")) {
+    $(this).find("input").prop("checked", true);
+  } else {
+    $(this).find("input").prop("checked", false);
+  }
 });
