bcbd32e315c8b90aeb0c1ded415fe68d2fad5a26
pukusyou <pukusho@gmail.com>
2023-02-06 15:26:40 +0900

    タスク見れるように

--- deal_json.py
+++ deal_json.py
@@ -1,13 +1,42 @@
 import json
 
-class load_json:
 
-    def __init__(self, filename):        
-        json_open = open('json/'+filename, 'r', encoding='utf-8')
-        self.dealer_list = ['prapor','Therapist','Skier','Peacekeeper','Mechanic', 'Ragman','Jaeger']
-        self.hideout_list = ['Bitcoin Farm','Booze Generator','Intelligence Center','Lavatory','Medstation','Nutrition Unit','Scav case','Water collector','Workbench'
-        ,'Air Filtering Unit','Generator','Heating','Illumination','Library','Rest Space','Security','Shooting range','Solar power',
-        'Stash','Vents']
+class load_json:
+    def __init__(self, filename):
+        json_open = open("json/" + filename, "r", encoding="utf-8")
+        self.dealer_list = [
+            "prapor",
+            "Therapist",
+            "Skier",
+            "Peacekeeper",
+            "Mechanic",
+            "Ragman",
+            "Jaeger",
+        ]
+        self.hideout_list = [
+            "Bitcoin Farm",
+            "Booze Generator",
+            "Intelligence Center",
+            "Lavatory",
+            "Medstation",
+            "Nutrition Unit",
+            "Scav case",
+            "Water collector",
+            "Workbench",
+            "Air Filtering Unit",
+            "Generator",
+            "Heating",
+            "Illumination",
+            "Library",
+            "Rest Space",
+            "Security",
+            "Shooting range",
+            "Solar power",
+            "Stash",
+            "Vents",
+            "Defective wall",
+            "Gym",
+        ]
         self.jsn = json.load(json_open)
 
     # dealerの持っているすべてのタスク名を返す。(空白を'_'に置き換えたものも返す)
@@ -17,9 +46,8 @@ class load_json:
         for key in self.jsn[dealer]:
             task_list.append(key)
         for task in task_list:
-            task_list_.append(task.replace(' ', '_'))
-        # print(list(zip(task_list,task_list_)))
-        return list(zip(task_list,task_list_))
+            task_list_.append(task.replace(" ", "_"))
+        return list(zip(task_list, task_list_))
 
     # dealerの持っているすべてのタスク名を返す。
     def get_dealer_task_name_plain(self, dealer):
@@ -41,20 +69,39 @@ class load_json:
         for task in tasks:
             for dealer in list:
                 for dealer_task in self.get_dealer_task_name_plain(dealer):
-                    if(task==dealer_task):
-                            dealer_list.append(dealer)
+                    if task == dealer_task:
+                        dealer_list.append(dealer)
         return dealer_list
-
 
     # 全ての中から残っているタスクを返す
     def get_sa_tasks(self, tasks):
-        tasks_name_list = [i for i in self.get_all_task_name_plain(self.dealer_list) if i not in tasks]
-        return [list(e) for e in zip(tasks_name_list, self.get_task_dealer(tasks_name_list, self.dealer_list))]
+        tasks_name_list = [
+            i
+            for i in self.get_all_task_name_plain(self.dealer_list)
+            if i not in tasks
+        ]
+        return [
+            list(e)
+            for e in zip(
+                tasks_name_list,
+                self.get_task_dealer(tasks_name_list, self.dealer_list),
+            )
+        ]
 
     def get_sa_hideout(self, tasks):
-        tasks_name_list = [i for i in self.get_all_task_name_plain(self.hideout_list) if i not in tasks]
-        return [list(e) for e in zip(tasks_name_list, self.get_task_dealer(tasks_name_list, self.hideout_list))]   
-        
+        tasks_name_list = [
+            i
+            for i in self.get_all_task_name_plain(self.hideout_list)
+            if i not in tasks
+        ]
+        return [
+            list(e)
+            for e in zip(
+                tasks_name_list,
+                self.get_task_dealer(tasks_name_list, self.hideout_list),
+            )
+        ]
+
     # 引数のタスクのwikiURLを返す。
     def get_task_url(self, dealer, task_name):
         return self.jsn[dealer][task_name]["wiki_url"]
@@ -63,72 +110,119 @@ class load_json:
     def get_task_item_fullname(self, dealer, task_name):
         fullnameList = []
         list = []
-        # print('this')
-        # print(self.jsn[dealer][task_name])
-        if('items' in self.jsn[dealer][task_name]):
+        if "items" in self.jsn[dealer][task_name]:
             for item in self.jsn[dealer][task_name]["items"]:
                 list.append(item)
             for i in list:
-                fullnameList.append(self.jsn[dealer][task_name]["items"][i]["full_name"])
+                fullnameList.append(
+                    self.jsn[dealer][task_name]["items"][i]["full_name"]
+                )
         return fullnameList
 
     def get_task_item_name(self, dealer, task_name):
         fullnameList = []
         list = []
-        if('items' in self.jsn[dealer][task_name]):
+        if "items" in self.jsn[dealer][task_name]:
             for item in self.jsn[dealer][task_name]["items"]:
                 list.append(item)
             for i in list:
-                fullnameList.append(self.jsn[dealer][task_name]["items"][i]["name"])
+                fullnameList.append(
+                    self.jsn[dealer][task_name]["items"][i]["name"]
+                )
         return fullnameList
 
     def get_task_item_num(self, dealer, task_name):
         fullnameList = []
         list = []
-        if('items' in self.jsn[dealer][task_name]):
+        if "items" in self.jsn[dealer][task_name]:
             for item in self.jsn[dealer][task_name]["items"]:
                 list.append(item)
             for i in list:
-                fullnameList.append(self.jsn[dealer][task_name]["items"][i]["num"])
+                fullnameList.append(
+                    self.jsn[dealer][task_name]["items"][i]["num"]
+                )
         return fullnameList
 
     def get_task_item_inRaid(self, dealer, task_name):
         fullnameList = []
         list = []
-        if('items' in self.jsn[dealer][task_name]):
+        if "items" in self.jsn[dealer][task_name]:
             for item in self.jsn[dealer][task_name]["items"]:
                 list.append(item)
             for i in list:
-                fullnameList.append(self.jsn[dealer][task_name]["items"][i]["inRaid"])
+                fullnameList.append(
+                    self.jsn[dealer][task_name]["items"][i]["inRaid"]
+                )
         return fullnameList
 
     def get_task_item_img(self, dealer, task_name):
         fullnameList = []
         list = []
-        if('items' in self.jsn[dealer][task_name]):
+        if "items" in self.jsn[dealer][task_name]:
             for item in self.jsn[dealer][task_name]["items"]:
                 list.append(item)
             for i in list:
-                fullnameList.append(self.jsn[dealer][task_name]["items"][i]["img"])
+                fullnameList.append(
+                    self.jsn[dealer][task_name]["items"][i]["img"]
+                )
         return fullnameList
 
     def get_task_item_category(self, dealer, task_name):
         fullnameList = []
         list = []
-        if('items' in self.jsn[dealer][task_name]):
+        if "items" in self.jsn[dealer][task_name]:
             for item in self.jsn[dealer][task_name]["items"]:
                 list.append(item)
             for i in list:
-                fullnameList.append(self.jsn[dealer][task_name]["items"][i]["category"])
+                fullnameList.append(
+                    self.jsn[dealer][task_name]["items"][i]["category"]
+                )
         return fullnameList
 
+    def get_task_wikiURL(self, dealer, task_name):
+        result = []
+        for num in range(len(self.get_task_item_fullname(dealer, task_name))):
+            result.append(self.jsn[dealer][task_name]["wiki_url"])
+        return result
+
+    def get_task_name(self, dealer, task_name):
+        result = []
+        for num in range(len(self.get_task_item_fullname(dealer, task_name))):
+            result.append(task_name)
+        return result
+
     def get_task_item_all(self, dealer, task_name):
-        return [list(e) for e in zip(self.get_task_item_fullname(dealer,task_name), self.get_task_item_name(dealer,task_name),
-        self.get_task_item_num(dealer,task_name), self.get_task_item_inRaid(dealer,task_name), self.get_task_item_img(dealer,task_name),self.get_task_item_category(dealer,task_name))]
+        result = [
+            list(e)
+            for e in zip(
+                self.get_task_item_fullname(dealer, task_name),
+                self.get_task_item_name(dealer, task_name),
+                self.get_task_item_num(dealer, task_name),
+                self.get_task_item_inRaid(dealer, task_name),
+                self.get_task_item_img(dealer, task_name),
+                self.get_task_item_category(dealer, task_name),
+                self.get_task_wikiURL(dealer, task_name),
+                self.get_task_name(dealer, task_name),
+            )
+        ]
+        print(result)
+        if len(result) > 0:
+            for a in range(len(result)):
+                result[a][6] = [result[a][6]]
+                result[a][7] = [result[a][7]]
+        return result
 
     def get_hideout_task_item_all(self, dealer, task_name):
-        return [list(e) for e in zip(self.get_task_item_fullname(dealer,task_name), self.get_task_item_name(dealer,task_name),
-        self.get_task_item_num(dealer,task_name), self.get_task_item_img(dealer,task_name))]
+        result = [
+            list(e)
+            for e in zip(
+                self.get_task_item_fullname(dealer, task_name),
+                self.get_task_item_name(dealer, task_name),
+                self.get_task_item_num(dealer, task_name),
+                self.get_task_item_img(dealer, task_name),
+            )
+        ]
+        return result
 
     def get_task_item_sum(self, tasks_name):
         lists = []
@@ -138,9 +232,16 @@ class load_json:
         length = len(lists)
         for num in range(length):
             next = num + 1
-            while(next<length):
-                if(lists[num][0]==lists[next][0] and lists[num][2]>0 and lists[next][2]>0):
-                    lists[num][2] = lists[num][2] + lists.pop(next)[2]
+            while next < length:
+                if (
+                    lists[num][0] == lists[next][0]
+                    and lists[num][2] > 0
+                    and lists[next][2] > 0
+                ):
+                    pop = lists.pop(next)
+                    lists[num][7] = lists[num][7] + pop[7]
+                    lists[num][6] = lists[num][6] + pop[6]
+                    lists[num][2] = lists[num][2] + pop[2]
                     length = length - 1
                 next = next + 1
         return lists
@@ -148,29 +249,34 @@ class load_json:
     def get_hideout_task_item_sum(self, tasks_name):
         lists = []
         for task_name in tasks_name:
-            for item in self.get_hideout_task_item_all(task_name[1], task_name[0]):
+            for item in self.get_hideout_task_item_all(
+                task_name[1], task_name[0]
+            ):
                 lists.append(item)
         length = len(lists)
         for num in range(length):
             next = num + 1
-            while(next<length):
-                if(lists[num][0]==lists[next][0] and lists[num][2]>0 and lists[next][2]>0):
+            while next < length:
+                if (
+                    lists[num][0] == lists[next][0]
+                    and lists[num][2] > 0
+                    and lists[next][2] > 0
+                ):
                     lists[num][2] = lists[num][2] + lists.pop(next)[2]
                     length = length - 1
                 next = next + 1
         return lists
 
-dj = load_json("hideout.json")
-print(dj.get_dealer_task_name("Scav case"))
+
 # print(dj.get_task_item_all('prapor','Debut'))
-    # 最初のkeyを取る
-    # for key in jsn:
-    #     print(key)
+# 最初のkeyを取る
+# for key in jsn:
+#     print(key)
 
-    # keyの値を取る
-    # print(jsn["The Punisher - Part 6"])
+# keyの値を取る
+# print(jsn["The Punisher - Part 6"])
 
-    # itemの一覧表示
-    # for key in jsn["The Punisher - Part 6"]:
-    #     for item in key["item"]:
-    #         print(item)
+# itemの一覧表示
+# for key in jsn["The Punisher - Part 6"]:
+#     for item in key["item"]:
+#         print(item)
--- script.py
+++ script.py
@@ -4,14 +4,17 @@ import urllib.request
 import time
 import deal_json as js
 import os
-class script:
 
-    def __init__(self, path): 
-        self.soup = bs4.BeautifulSoup(open(path,encoding='utf-8'), 'html.parser')
+
+class script:
+    def __init__(self, path):
+        self.soup = bs4.BeautifulSoup(
+            open(path, encoding="utf-8"), "html.parser"
+        )
 
     def get_Task_name(self):
         list = []
-        list = self.soup.find('ul',class_="list1").select('li>a')
+        list = self.soup.find("ul", class_="list1").select("li>a")
         task_list = []
         for task in list:
             task_list.append(task.string)
@@ -19,24 +22,35 @@ class script:
 
     def get_task_url(self):
         list = []
-        list = self.soup.find('ul',class_="list1").select('li>a')
+        list = self.soup.find("ul", class_="list1").select("li>a")
         task_list = []
         for task in list:
-            task_list.append(task.get('href'))
+            task_list.append(task.get("href"))
         return task_list
 
     def write_JSON(self):
         js = "{"
-        for num in range(len(self.get_Task_name())):    
-            js = js + '"' + self.get_Task_name()[num] + '":{"wiki_url":"' + self.get_task_url()[num] + '",\n'
-            js = js + '"items":{"item1":{"full_name":"","name":"","num":0,"inRaid":true,"img":""}}},\n'
-        js = js + '}'
-        
+        for num in range(len(self.get_Task_name())):
+            js = (
+                js
+                + '"'
+                + self.get_Task_name()[num]
+                + '":{"wiki_url":"'
+                + self.get_task_url()[num]
+                + '",\n'
+            )
+            js = (
+                js
+                + '"items":{"item1":{"full_name":"","name":"","num":0,"inRaid":true,"img":""}}},\n'
+            )
+        js = js + "}"
+
         print(js)
-        f = open('test.json', 'w', encoding='UTF-8')
+        f = open("test.json", "w", encoding="UTF-8")
         f.writelines(js)
 
-def replace_str(prain, rep,filename):
+
+def replace_str(prain, rep, filename):
     file_name = "json/" + filename
 
     with open(file_name, encoding="utf-8") as f:
@@ -49,44 +63,53 @@ def replace_str(prain, rep,filename):
     with open(file_name, mode="w", encoding="utf-8") as f:
         f.write(data_lines)
 
+
 def download_img(dealer, filename):
     js = deal_json.load_json(filename)
     img = []
-    name= []
+    name = []
     for task in js.get_dealer_task_name_plain(dealer):
-        img = img + js.get_task_item_img(dealer,task)
+        img = img + js.get_task_item_img(dealer, task)
     for taska in js.get_dealer_task_name_plain(dealer):
-        name = name + js.get_task_item_fullname(dealer,taska)
+        name = name + js.get_task_item_fullname(dealer, taska)
     name_ = []
     # os.mkdir('static/img/'+ dealer)
     for p in name:
-        name_.append(p.replace(' ','_'))
+        name_.append(p.replace(" ", "_"))
     for n in range(len(img)):
-        if(img[n].find("http")!=-1):
-            save_path='static/img/'+ dealer + '/' + name_[n] +'.png'
-            opener=urllib.request.build_opener()
-            opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        if img[n].find("http") != -1:
+            save_path = "static/img/" + dealer + "/" + name_[n] + ".png"
+            opener = urllib.request.build_opener()
+            opener.addheaders = [
+                (
+                    "User-Agent",
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36",
+                )
+            ]
             urllib.request.install_opener(opener)
             urllib.request.urlretrieve(img[n], save_path)
-            replace_str(img[n],"../../"+save_path,"hideout.json")
-            print(img[n] + ' >>> ' + save_path)
+            replace_str(img[n], "../../" + save_path, "hideout.json")
+            print(img[n] + " >>> " + save_path)
             time.sleep(2)
 
+
 def add_str(path, yoso, yoso2, sinyoso):
-    with open(path, "r",encoding="utf-8") as file:
+    with open(path, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
     with open(path, "w", encoding="utf-8") as file:
         for line in lines:
-            if yoso in line and (yoso2 not in line and 'Key' not in line):
-                file.write(line+',')
-                file.write(sinyoso+'\n')
+            if yoso in line and (yoso2 not in line and "Key" not in line):
+                file.write(line + ",")
+                file.write(sinyoso + "\n")
             else:
                 file.write(line)
+
+
 # hideout_list = ['Bitcoin Farm','Booze Generator','Intelligence Center','Lavatory','Medstation','Nutrition Unit','Scav case','Water collector','Workbench'
 #         ,'Air Filtering Unit','Generator','Heating','Illumination','Library','Rest Space','Security','Shooting range','Solar power',
 #         'Stash','Vents']
-download_img("Mechanic","task.json")
+download_img("Gym", "hideout.json")
 # def write_txt(list):
 #     f = open('item.txt', 'w')
 #     for txt in list:
